e6b99190f1c8bfdb57039fb536ac1cbf0696636d
Артем <111086481+ArTdrums@users.noreply.github.com>
2024-02-13 20:16:59 +0300

    svetofor_tensorflow.py

--- svetofor_tensorflow.py
+++ svetofor_tensorflow.py
@@ -18,7 +18,6 @@ model = Sequential([
 
 ])
 
-
 model.compile(optimizer='sgd', loss='mse', metrics='mae')
 
 # запускаем обучение для вертикального движения
@@ -38,22 +37,22 @@ for i in df_gorixont:
         'пердсказания': np.squeeze(model.predict(i)),
     }))
 
-# запускаем обучение для поворота на права снизу вверх
+# запускаем обучение для поворота на право снизу вверх
 
-for i in df_povorot_1_ssnizy_vverh:
-    print(model.fit(i, y_povorot_1_snizy_vverh, epochs=100))
+for i in df_povorot_1_ssnizy_vverh_vertikal:
+    print(model.fit(i, y_povorot_1_snizy_vverh_vertikal, epochs=100))
     print(pd.DataFrame({
-        'верное значение': np.squeeze(y_povorot_1_snizy_vverh),
+        'верное значение': np.squeeze(y_povorot_1_snizy_vverh_vertikal),
         'пердсказания': np.squeeze(model.predict(i)),
     }))
 
-# запускаем обучение для поворота на права сверху вниз
+# запускаем обучение для поворота на право сверху вниз
 
 
-for i in df_povorot_1_sverhy_vnis:
-    print(model.fit(i, y_povorot_1_snizy_vverh, epochs=100))
+for i in df_povorot_1_sverhy_vnis_vertikal:
+    print(model.fit(i, y_povorot_1_sverhy_vnis_vertikal, epochs=100))
     print(pd.DataFrame({
-        'верное значение': np.squeeze(y_povorot_1_sverhy_vnis),
+        'верное значение': np.squeeze(y_povorot_1_sverhy_vnis_vertikal),
         'пердсказания': np.squeeze(model.predict(i)),
     }))
 
@@ -66,6 +65,23 @@ for i in df_pesh:
         'пердсказания': np.squeeze(model.predict(i)),
     }))
 
+# запускаем обучение для поворота на право при горизонтальном движении справа налево
+
+for i in df_povorot_1_sverhy_vnis_gorizont:
+    print(model.fit(i, y_povorot_1_sverhy_vnis_gorizont, epochs=100))
+    print(pd.DataFrame({
+        'верное значение': np.squeeze(y_povorot_1_sverhy_vnis_gorizont),
+        'пердсказания': np.squeeze(model.predict(i)),
+    }))
+
+# запускаем обучение для поворота на право при горизонтальном движении справа налево
+for i in df_X_povorot_2_sverhy_vnis_gorizont:
+    print(model.fit(i, y_povorot_2_sverhy_vnis_gorizont, epochs=100))
+    print(pd.DataFrame({
+        'верное значение': np.squeeze(y_povorot_2_sverhy_vnis_gorizont),
+        'пердсказания': np.squeeze(model.predict(i)),
+    }))
+
 # тестирование нейронной сети
 
 ''' округление доцелого числа если надо
@@ -74,5 +90,8 @@ b = np.array([int(i) for i in np.squeeze(model.predict(X))])
 print('было ', w1, w0, '\n', 'стало', w2, w3)
 '''
 # тестирование нейронной сети
+test_net = [6, 5, 8, 7, 22, 18, 22, 21, 17, 19, 19, 22]
+for i, item in enumerate(model.predict(np.array(test_net) / 25.0 * 0.99 + 0.01)):
+    print(f'светофор под индексом {i}, вероятность включения {item}')
 
-print(model.predict(np.array([6, 5, 8, 7, 22, 18, 22, 21, 17, 19, 19, 22]) / 25.0 * 0.99 + 0.01))
+
